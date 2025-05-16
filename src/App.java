73bc47380387fbbda47cc357b5bbf391c3c72358
Raj Kumar Mishra <119384637+princeksp@users.noreply.github.com>
2025-05-16 10:38:29 +0530

    Update App.java

--- src/App.java
+++ src/App.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DoctorPatientApplication {
+public class App {
     public static void main(String[] args) {
-        SpringApplication.run(DoctorPatientApplication.class, args);
+        SpringApplication.run(App.class, args);
     }
 }
